e25c43da5deed9a47b9bf862e6b8f42688406c79
Xskuro404 <149703021+Xskuro404@users.noreply.github.com>
2024-04-28 04:15:56 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 clear
-figlet "RixXze"
+figlet "MRXPLOIT"
 echo "RANK BOOSTER V.I.P V12"
 echo "Mobile Legends S32"
 sleep 1
@@ -9,41 +9,41 @@ sleep 1
 echo ""
 read -p " Password : " password
 
-if [ $password = wI8Cb6sSEe ]
+if [ $password = #+$+$+$!$($!;32++2+2!3+#+#+2!2 ]
 then
 	clear
-	figlet "RixXze"
+	figlet "MRXPLOIT"
 	echo "RANK BOOSTER V11"
 	echo "Mobile Legends S32"
 	echo ""
-	echo " Password : wI8Cb6sSEe -true"
+	echo " Password : #+$+$+$!$($!;32++2+2!3+#+#+2!2 -true"
 	sleep 1
 	
 	sleep 2 && echo Waitting for setup...
-	sleep 3 && clear && figlet RixXze
+	sleep 3 && clear && figlet MRXPLOIT RANK BOOSTER
 	sleep 0.5 && echo Setup...    0%
 	sleep 1 && echo [■□□□□□□□□□□]
-	sleep 1 && clear && figlet RixXze && echo Setup...  12%
+	sleep 1 && clear && figlet MRXPLOIT && echo Setup...  12%
 	sleep 0 && echo [■■□□□□□□□□□]
-	sleep 1 && clear && figlet RixXze && echo Setup...  28%
+	sleep 1 && clear && figlet MRXPLOIT && echo Setup...  28%
 	sleep 0 && echo [■■■□□□□□□□□]
-	sleep 1 && clear && figlet RixXze && echo Setup...  31%
+	sleep 1 && clear && figlet MRXPLOIT && echo Setup...  31%
 	sleep 0 && echo [■■■■□□□□□□□]
-	sleep 1 && clear && figlet RixXze && echo Setup...  39%
+	sleep 1 && clear && figlet MRXPLOIT && echo Setup...  39%
 	sleep 0 && echo [■■■■■□□□□□□]
-	sleep 1 && clear && figlet RixXze && echo Setup...  57%
+	sleep 1 && clear && figlet MRXPLOIT && echo Setup...  57%
 	sleep 0 && echo [■■■■■■□□□□□]
-	sleep 1 && clear && figlet RixXze && echo Setup...  61%
+	sleep 1 && clear && figlet MRXPLOIT && echo Setup...  61%
 	sleep 0 && echo [■■■■■■■□□□□]
-	sleep 1 && clear && figlet RixXze && echo Setup...  70%
+	sleep 1 && clear && figlet MRXPLOIT && echo Setup...  70%
 	sleep 0 && echo [■■■■■■■■□□□]
-	sleep 1 && clear && figlet RixXze && echo Setup...  84%
+	sleep 1 && clear && figlet MRXPLOIT && echo Setup...  84%
 	sleep 0 && echo [■■■■■■■■■□□]
-	sleep 1 && clear && figlet RixXze && echo Setup...  95%
+	sleep 1 && clear && figlet MRXPLOIT && echo Setup...  95%
 	sleep 0 && echo [■■■■■■■■■■□]
-	sleep 1 && clear && figlet RixXze && echo Setup... 100%
+	sleep 1 && clear && figlet MRXPLOIT && echo Setup... 100%
 	sleep 0 && echo [■■■■■■■■■■]
-	sleep 0.5 && clear && figlet RixXze && echo Setup... Completed
+	sleep 0.5 && clear && figlet MRXPLOIT && echo Setup... Completed
 	sleep 0 && echo [■■■■■■■■■■]ðŸ’¥
 	am start --user 0 -n com.mobile.legends/com.moba.unityplugin.MobaGameMainActivityWithExtractor
 
